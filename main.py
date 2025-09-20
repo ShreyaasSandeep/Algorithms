e34edf6389a347cd0b0dd579c0b5bdce06636b39
Shreyaas Sandeep <shreyaassandeep@Shreyaass-MacBook-Pro.local>
2025-09-20 17:15:17 +0100

    Changed my position to become proportional to momentum

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 data = yf.download("AAPL", start="2018-01-01", end="2025-01-01", auto_adjust=True)
 
 data["returns"] = data["Close"].pct_change()
-data["momentum"] = data["Close"].pct_change(23)
-data["position"] = np.where(data["momentum"] > 0, 1, 0)
+data["momentum"] = data["Close"].pct_change(20)
+#data["position"] = np.where(data["momentum"] > -0.01, 1, 0)
+data["position"] = data["momentum"].clip(-0.01, 0.01) / 0.01
 data["strategy"] = data["position"].shift(1) * data["returns"]
 
 (1 + data[["returns", "strategy"]]).cumprod().plot(figsize=(10,6))
